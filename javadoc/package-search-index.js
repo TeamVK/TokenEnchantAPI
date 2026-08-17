44f7b368c8282e07e73584ea2f5b9e69aa63cb73
vk2gpz <2870239+vk2gpz@users.noreply.github.com>
2026-08-18 09:16:13 +1000

    released 23.12.5.

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.vk2gpz.tokenenchant.api"},{"l":"com.vk2gpz.tokenenchant.event"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"com.vk2gpz.tokenenchant.api"},{"l":"com.vk2gpz.tokenenchant.event"}];updateSearchResults();
